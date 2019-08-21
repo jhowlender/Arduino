c99f9a3c76f59c5716d13894954b2a755c3b4d46
jhowlender <54338675+jhowlender@users.noreply.github.com>
2019-08-20 22:30:07 -0300

    modificaçao avançada do projeto supermaquina

--- supermaquina/supermaquina.ino
+++ supermaquina/supermaquina.ino
@@ -13,20 +13,36 @@ void setup() {
 
 void loop() {
   digitalWrite(13, HIGH);
-  delay(100);
+  delay(20);
   digitalWrite(13, LOW);
-  delay(100);
+  delay(20);
   digitalWrite(12, HIGH);
-  delay(100);
+  delay(30);
   digitalWrite(12, LOW);
-  delay(100);
+  delay(30);
   digitalWrite(11, HIGH);
-  delay(100);
+  delay(40);
   digitalWrite(11, LOW);
-  delay(100);
+  delay(40);
   digitalWrite(10, HIGH);
-  delay(100);
+  delay(50);
   digitalWrite(10, LOW);
-  delay(100);
+  delay(50);
+  digitalWrite(10, HIGH);
+  delay(50);
+  digitalWrite(10, LOW);
+  delay(50);
+  digitalWrite(11, HIGH);
+  delay(40);
+  digitalWrite(11, LOW);
+  delay(40);
+  digitalWrite(12, HIGH);
+  delay(30);
+  digitalWrite(12, LOW);
+  delay(30);
+  digitalWrite(13, HIGH);
+  delay(20);
+  digitalWrite(13, LOW);
+  delay(20);
   
 }
